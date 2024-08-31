d7ab1263a603ec329b55dbe5da4fe34f88795037
zakir <azizizakir449@gmail.com>
2024-08-31 19:31:57 +0500

    my commit

--- 02_if_elif_else_ladder.py
+++ 02_if_elif_else_ladder.py
@@ -1,12 +1,16 @@
 a = int(input("Enter your age: "))
 
-# If else
-
+# If elif alse ladder
 if(a>18):
     print("tou are above the age of consent")
     print("Good for you")
 
+elif(a<0):
+    print("your are entering an invalid age")
 
+elif(a==0):
+    print("you are entering 0 which is not a valed age")
+    
 else:
     print("you areblow the page of consent")
 
