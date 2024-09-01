4e5fb5e409ff5802e6404f0147e2cd08774c4252
zakir <azizizakir449@gmail.com>
2024-09-01 22:16:32 +0500

    my commit

---
+++ 01_problem1.py
@@ -0,0 +1,16 @@
+a1 = int(input("Enter number 1: "))
+a2 = int(input("Enter number 2: "))
+a3 = int(input("Enter number 3: "))
+a4 = int(input("Enter number 4: "))
+
+if(a1>a2 and a1>a3 and a1>a4):
+    print("Greatest numbr is a1", a1)
+
+if(a2>a1 and a2>a3 and a2>a4):
+    print("Greatest numbr is a2",  a2)
+
+if(a3>a1 and a3>a2 and a3>a4):
+    print("Greatest numbr is a3",  a3)
+
+if(a4>a1 and a4>a2 and a4>a3):
+    print("Greatest numbr is a4",  a4)
---
+++ 03_multiple_if_statem.py
@@ -0,0 +1,17 @@
+a = int(input("Enter your age: "))
+
+# If elif alse ladder
+if(a%2 == 0):
+    print("a is even")
+if(a>18):
+    print("tou are above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("your are entering an invalid age")
+    
+else:
+    print("you areblow the page of consent")
+
+
+print("ENd of program")
