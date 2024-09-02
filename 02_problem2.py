33e216a73723602a10591f9d2494f96ca843d682
zakir <azizizakir449@gmail.com>
2024-09-02 23:22:46 +0500

    my commit

--- 02_problem2.py
+++ 02_problem2.py
@@ -1,3 +1,12 @@
 marks1 = int(input("Enter Marks 1: "))
 marks2 = int(input("Enter Marks 2: "))
 marks3 = int(input("Enter Marks 3: "))
+
+# Check for total percentage
+total_percentage = (100*(marks1 + marks2 + marks3))/300
+
+if(total_percentage>=35 and marks1>33 and marks2>33 and marks3>33):
+    print("you are passed", total_percentage)
+
+else:
+    print("you failed, try again nest year:", total_percentage)
