7628c4ccab81d6de1fbd53e8124678d90a6f2e3e
zakir <azizizakir449@gmail.com>
2024-09-04 21:13:23 +0500

    my commit

---
+++ 07_problem7.py
@@ -0,0 +1,9 @@
+post = input("enter the post: ")
+
+
+
+if("Zakir".lower() in post.lower()):
+    print("this post is talking about zakir")
+
+else:
+    print("This post is not talking about zakir")
